fd088d37c3a5e677c6af16dae4418837660950ed
Zears14 <79321017+Zears14@users.noreply.github.com>
2023-05-20 18:00:42 +0700

    Making Code Less Shit 1

--- Picrypt/Program.cs
+++ Picrypt/Program.cs
@@ -7,96 +7,96 @@ using System.Windows.Forms;
 
 namespace Picrypt
 {
-	internal static class Program
-	{
-		public static string binPath;
-		public static string keypairPath;
-		public static string publicXmlPath;
-		[STAThread]
-		private static void Main()
-		{
-			Application.EnableVisualStyles();
-			Application.SetHighDpiMode(HighDpiMode.SystemAware);
-			Application.SetCompatibleTextRenderingDefault(false);
-			binPath = Path.Combine(Directory.GetCurrentDirectory(), "bin");
-			keypairPath = Path.Combine(binPath, "keypair");
-			publicXmlPath = Path.Combine(keypairPath, "public.xml");
-			string privateXmlPath = Path.Combine(keypairPath, "private.p8");
-			string passphrase = null;
+    internal static class Program
+    {
+        public static string binPath;
+        public static string keypairPath;
+        public static string publicXmlPath;
 
-			if (!Directory.Exists(binPath))
-			{
-				Directory.CreateDirectory(binPath);
-			}
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.SetCompatibleTextRenderingDefault(false);
+            binPath = Path.Combine(Directory.GetCurrentDirectory(), "bin");
+            keypairPath = Path.Combine(binPath, "keypair");
+            publicXmlPath = Path.Combine(keypairPath, "public.xml");
+            string privateXmlPath = Path.Combine(keypairPath, "private.p8");
+            string passphrase = null;
 
-			if (!Directory.Exists(keypairPath))
-			{
-				Directory.CreateDirectory(keypairPath);
-			}
+            if (!Directory.Exists(binPath))
+            {
+                Directory.CreateDirectory(binPath);
+            }
 
-			if (!File.Exists(publicXmlPath) || !File.Exists(privateXmlPath))
-			{
-				try
-				{
-					// Generate a new RSA key pair
-					using (RSA rsa = RSA.Create())
-					{   // Export the public key to XML
-						rsa.KeySize = 4096;
-						string publicKey = rsa.ToXmlString(false);
-						File.WriteAllText(publicXmlPath, publicKey);
+            if (!Directory.Exists(keypairPath))
+            {
+                Directory.CreateDirectory(keypairPath);
+            }
 
-						// Export the private key to XML encrypted with a passphrase
-						using (var form = new Form())
-						{
-							form.Text = "Enter Passphrase";
-							form.FormBorderStyle = FormBorderStyle.FixedDialog;
-							form.MaximizeBox = false;
-							form.MinimizeBox = false;
-							form.StartPosition = FormStartPosition.CenterParent;
-							var passwordLabel = new Label() { Left = 20, Top = 20, Text = "Passphrase:" };
-							var passwordTextBox = new TextBox() { Left = 100, Top = 20, Width = 200, PasswordChar = '*' };
-							var cancelButton = new Button() { Text = "Cancel", Left = 235, Width = 70, Top = 60 };
-							var okButton = new Button() { Text = "OK", Left = 160, Width = 70, Top = 60 };
-							okButton.DialogResult = DialogResult.OK;
-							cancelButton.DialogResult = DialogResult.Cancel;
-							form.AcceptButton = okButton;
-							form.CancelButton = cancelButton;
-							form.ClientSize = new Size(320, 100);
-							form.Controls.Add(okButton);
-							form.Controls.Add(cancelButton);
-							form.Controls.Add(passwordLabel);
-							form.Controls.Add(passwordTextBox);
-							if (form.ShowDialog() == DialogResult.OK)
-							{
-								passphrase = passwordTextBox.Text;
-							}
-							else if (form.ShowDialog() == DialogResult.Cancel)
-							{
-								Application.Exit();
-							}
-						}
+            if (!File.Exists(publicXmlPath) || !File.Exists(privateXmlPath))
+            {
+                try
+                {
+                    // Generate a new RSA key pair
+                    using (RSA rsa = RSA.Create())
+                    {   // Export the public key to XML
+                        rsa.KeySize = 4096;
+                        string publicKey = rsa.ToXmlString(false);
+                        File.WriteAllText(publicXmlPath, publicKey);
 
-						var pbeParams = new PbeParameters(PbeEncryptionAlgorithm.Aes256Cbc, HashAlgorithmName.SHA512, 10000);
+                        // Export the private key to XML encrypted with a passphrase
+                        using (var form = new Form())
+                        {
+                            form.Text = "Enter Passphrase";
+                            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+                            form.MaximizeBox = false;
+                            form.MinimizeBox = false;
+                            form.StartPosition = FormStartPosition.CenterParent;
+                            var passwordLabel = new Label() { Left = 20, Top = 20, Text = "Passphrase:" };
+                            var passwordTextBox = new TextBox() { Left = 100, Top = 20, Width = 200, PasswordChar = '*' };
+                            var cancelButton = new Button() { Text = "Cancel", Left = 235, Width = 70, Top = 60 };
+                            var okButton = new Button() { Text = "OK", Left = 160, Width = 70, Top = 60 };
+                            okButton.DialogResult = DialogResult.OK;
+                            cancelButton.DialogResult = DialogResult.Cancel;
+                            form.AcceptButton = okButton;
+                            form.CancelButton = cancelButton;
+                            form.ClientSize = new Size(320, 100);
+                            form.Controls.Add(okButton);
+                            form.Controls.Add(cancelButton);
+                            form.Controls.Add(passwordLabel);
+                            form.Controls.Add(passwordTextBox);
+                            if (form.ShowDialog() == DialogResult.OK)
+                            {
+                                passphrase = passwordTextBox.Text;
+                            }
+                            else if (form.ShowDialog() == DialogResult.Cancel)
+                            {
+                                Application.Exit();
+                            }
+                        }
 
-						byte[] encryptedPrivateKey = rsa.ExportEncryptedPkcs8PrivateKey(Encoding.UTF8.GetBytes(passphrase), pbeParams);
-						File.WriteAllBytes(privateXmlPath, encryptedPrivateKey);
-						Console.WriteLine("Success");
-					}
-				}
-				catch (CryptographicException e)
-				{
-					MessageBox.Show(e.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				}
-				catch (Exception e)
-				{
-					MessageBox.Show(e.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				}
-				finally
-				{
-				}
-			}
-			Application.Run(new Main());
-		}
+                        var pbeParams = new PbeParameters(PbeEncryptionAlgorithm.Aes256Cbc, HashAlgorithmName.SHA512, 10000);
 
-	}
+                        byte[] encryptedPrivateKey = rsa.ExportEncryptedPkcs8PrivateKey(Encoding.UTF8.GetBytes(passphrase), pbeParams);
+                        File.WriteAllBytes(privateXmlPath, encryptedPrivateKey);
+                        Console.WriteLine("Success");
+                    }
+                }
+                catch (CryptographicException e)
+                {
+                    MessageBox.Show(e.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                }
+            }
+            Application.Run(new Main());
+        }
+    }
 }
